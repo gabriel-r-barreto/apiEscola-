2d2eadf8d455602b62808315c6cc431b3d9dd056
gabriel-r-barreto <58337171+gabriel-r-barreto@users.noreply.github.com>
2023-04-12 23:14:52 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000; // Porta do servidor, você pode alterar para qualquer valor 
 const valorBoll = false
