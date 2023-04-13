1cda871a3d8a511b74ea089a44fcc487739478ef
gabriel-r-barreto <58337171+gabriel-r-barreto@users.noreply.github.com>
2023-04-13 17:55:50 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+
+var mensagens = [];
 
 const PORT = process.env.PORT || 3000; // Porta do servidor, você pode alterar para qualquer valor 
 let name;
@@ -32,6 +36,43 @@ app.post('/api/boolean', (req, res) => {
 
 app.get('/api/boolean', (req, res) => {
   res.json({ valor: name || "", escolar: school || "", endereco: adress || ""});
+});
+
+
+
+// Rota para obter todas as mensagens
+app.get('/api/mensagens', (req, res) => {
+  res.json(mensagens);
+});
+
+
+app.get('/api/apagar/mensagens', (req, res) => {
+  mensagens = []
+  res.json(mensagens);
+});
+
+// Rota para obter mensagens de um usuário específico
+app.get('/api/mensagens/:usuarioId', (req, res) => {
+  const usuarioId = req.params.usuarioId;
+  const mensagensDoUsuario = mensagens.filter(mensagem => mensagem.remetente === usuarioId || mensagem.destinatario === usuarioId);
+  res.json(mensagensDoUsuario);
+});
+
+// Rota para criar uma nova mensagem
+app.post('/api/mensagens', (req, res) => {
+  const novaMensagem = req.body;
+  mensagens.push(novaMensagem);
+  res.json(novaMensagem);
+});
+
+// Rota para excluir uma mensagem
+app.delete('/api/mensagens/:mensagemId', (req, res) => {
+  const mensagemId = req.params.mensagemId;
+  const mensagemIndex = mensagens.findIndex(mensagem => mensagem.id === mensagemId);
+  if (mensagemIndex >= 0) {
+    mensagens.splice(mensagemIndex, 1);
+  }
+  res.json({ message: 'Mensagem excluída com sucesso' });
 });
 
 
